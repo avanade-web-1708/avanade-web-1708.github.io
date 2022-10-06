e785b52cf0558fbf97f14d725460ee2bdc48f357
Alexsander Silva <alexsander.silva14@live.com>
2022-10-06 15:30:26 -0300

    Deploy

--- assets/js/loja.js
+++ assets/js/loja.js
@@ -3,28 +3,24 @@
 //Trazer apenas 10 produtos
 
 const produtos = async () => {
-    const options = {
-        method: 'GET',
-        mode: 'cors',
-        cache: 'default'
-    }
-    const busca = await fetch('https://dummyjson.com/products', options)
-    const json = await busca.json()
-    for(let i = 0 ; i < 30 ; i++){
-        const div = document.createElement('div')
-        div.setAttribute('class', 'produtos')
-        div.innerHTML = `<h1 id="title${i}"></h1><p id="description${i}"></p><div id="foto${i}"></div><h2 id="price${i}"></h2><button>Comprar</button>`
-        document.querySelector('main').appendChild(div)
-        if(document.querySelector('#title' + i) && document.querySelector('#description' + i) && document.querySelector('#price' + i) ){
-            document.querySelector('#title' + i).innerHTML = json.products[i].title
-            document.querySelector('#description' + i).innerHTML = json.products[i].description
-            document.querySelector('#price' + i).innerHTML = 'R$' + json.products[i].price
-            document.querySelector('#foto' + i).innerHTML = `<img src="${json.products[i].images[0]}" alt="" width="167" heigth="88" class="foto">` 
-        }
-    }
+  const options = {
+    method: "GET",
+    mode: "cors",
+    cache: "default",
+  };
+  const busca = await fetch("https://dummyjson.com/products", options);
+  const json = await busca.json();
+  for (let i = 0; i < 30; i++) {
+    const div = document.createElement("div");
+    div.setAttribute("class", "produtos");
+    div.innerHTML = `<h1 id="title${i}"></h1><p id="description${i}"></p><div id="foto${i}"></div><h2 id="price${i}"></h2><button>Comprar</button>`;
+    document.querySelector("main").appendChild(div);
 
-    
-}
+    document.querySelector("#title" + i).innerHTML = json.products[i].title;
+    document.querySelector("#description" + i).innerHTML = json.products[i].description;
+    document.querySelector("#price" + i).innerHTML = "R$" + json.products[i].price;
+    document.querySelector("#foto" + i).innerHTML = `<img src="${json.products[i].images[0]}" alt="" width="167" heigth="88" class="foto">`;
+  }
+};
 
-
-produtos()
+produtos();
